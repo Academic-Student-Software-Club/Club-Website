9380c0c6db03969e4d9ef181655f2e71e6e904d7
Tmanhertzog <tmanhertzog@gmail.com>
2025-09-29 20:57:54 -0700

    changed proj colors and added modal

---
+++ scripts/projects.js
@@ -0,0 +1,14 @@
+function openModal_proj0() {
+  document.getElementById("emptyModal").style.display = "flex";
+}
+function closeModal_proj0() {
+  document.getElementById("emptyModal").style.display = "none";
+}
+
+function openModal_proj1() {
+  document.getElementById("gameModal").style.display = "flex";
+}
+function closeModal_proj1() {
+  document.getElementById("gameModal").style.display = "none";
+}
+
